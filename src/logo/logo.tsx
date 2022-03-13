646c01f7508e803784c4daa44db2f8ac470785fb
Denis Churbanov <4urbanoff@gmail.com>
2022-03-13 11:10:19 +0300

    add css-variables demo

--- src/logo/logo.tsx
+++ src/logo/logo.tsx
@@ -4,7 +4,7 @@ import { Div, Span } from './logo.css'
 export const Logo = () => {
     return (
         <Div overlay>
-            <Div logo>
+            <Div logo __size="220px">
                 <Span candy>Candy</Span>
                 <Span slogan>next react styling level</Span>
             </Div>
